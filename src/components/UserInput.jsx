dfdff68aedec738090c101b30a1d9bb16368d14f
HawraaYounes <hawraayounes0@gmail.com>
2024-05-19 14:54:03 +0300

    user input component two way binding

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
+import { useState } from "react";
 
 const UserInput = () => {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
-      <div className='input-group'>
+      <div className="input-group">
         <p>
-            <label>Initial Investment</label>
-            <input type="number" required></input>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onClick={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          ></input>
         </p>
         <p>
-            <label>Annual Investment</label>
-            <input type="number" required></input>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onClick={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          ></input>
         </p>
       </div>
-      <div className='input-group'>
+      <div className="input-group">
         <p>
-            <label>Expected Return</label>
-            <input type="number" required></input>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onClick={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          ></input>
         </p>
         <p>
-            <label>Duration</label>
-            <input type="number" required></input>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onClick={(event) => handleChange("duration", event.target.value)}
+          ></input>
         </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default UserInput
+export default UserInput;
