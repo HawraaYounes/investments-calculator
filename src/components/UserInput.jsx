c53f0260a4883ee3cf1fe3c01012d8774af9eb55
HawraaYounes <hawraayounes0@gmail.com>
2024-05-19 14:22:39 +0300

    Added user input component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const UserInput = () => {
+  return (
+    <section id="user-input">
+      <div className='input-group'>
+        <p>
+            <label>Initial Investment</label>
+            <input type="number" required></input>
+        </p>
+        <p>
+            <label>Annual Investment</label>
+            <input type="number" required></input>
+        </p>
+      </div>
+      <div className='input-group'>
+        <p>
+            <label>Expected Return</label>
+            <input type="number" required></input>
+        </p>
+        <p>
+            <label>Duration</label>
+            <input type="number" required></input>
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default UserInput
